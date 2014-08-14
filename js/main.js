ecb820b2d3f312a615ee6378b3c3bd5429eac52e
k.pantoja23@yahoo.com <>
2014-08-14 15:49:25 +0000

    preload completeddd

--- js/main.js
+++ js/main.js
@@ -1,85 +1,54 @@
-//Initialize Phaser. Create a 400x490px game
+//Intialize Phaser Engine. create 400x490px. game!
 
-var game = new Phaser.Game(400,490, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(400, 490, Phaser.AUTO,"gameDiv");
 
-//Create our 'main' state that will contain the game
-//This is the body of the game itself. It should contain all code relevant to the game running
+//create our 'main' state that will contian the game
+//this is the body of the game itself
+//it should contain all code related to the game itself
 
 var mainState = {
+  preload: function () {
+   //This function will execute at the beginning
+   //which is where we'll load our art assets for the game  
+   //set the background color of the game
   
-  preload: function() {
-    //This function will execute at the beginning
-    //which is where we'll load our assets for the game
-    
-    //Set/Change the background color of the main stage
     game.stage.backgroundColor = "#71c5cf";
-    
-    //Let's load the bird sprite
+
+
     game.load.image('bird', 'assets/bird.png');
     
-  },
-  
-  
-  create: function(){
+    game.load.image('pipe', 'assets/pipe.png');
     
-    //This function is called after the preload function
-    //This is where we'll set up the game, place the assets, display, etc...
-    
-    //Let's start up the physics system
+    },
+ 
+ create: function () {
+   //This is calls right after preload function 
+   //this is where we set up the game assets from earlier
+   
+   
+   
     game.physics.startSystem(Phaser.Physics.ARCADE);
     
-    //We want to display the bird on the screen now
-    this.bird = this.game.add.sprite(200, 245, 'bird');
+    this.bird = this.game.add.sprite(100, 245,'bird');
     
-    //Now that we have the bird loaded and the physics engine started
-    //we need to add gravity to the bird in order for it to actually
-    //fall
+    
+    //Now that we have a bird and gravity...we need to tell the bird to react to the gravity
+    
     
     game.physics.arcade.enable(this.bird);
-    this.bird.body.gravity.y = 10;
     
-    //Next, we need to make sure the game knows what to do whenever the
-    //spacebar key is pressed, so we'll add that here
+    this.bird.body.gravity.y = 1000;   
+   
+   
+   
+ },
+ 
+ update: function () {
+   //this function runs 60 times per second
+ },
     
-    var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    spaceKey.onDown.add(this.jump, this);
-    
-  },
-  
-  update: function(){
-    //This function is called 60 times per second
-    //It contains the game's logic
-    //This isn't perfect, as it doesn't allow for lower hardware frame rates
-    
-    //Let's check to see where the bird is out on the screen
-    //If it's off the screen or not in the world then restart the game
-    if (this.bird.inWorld == false)
-      this.restartGame();
-  },
-  
-  //This function makes the bird jump
-  jump: function(){
-    
-    //Let's add a vertical velocity to the bird
-    //Or a jump for better explanation!
-    
-    this.bird.body.velocity.y = -350;
-  },
-  
-  restartGame: function(){
-    
-    //Start the 'mainState' section again, which restarts the game
-    
-    game.state.start('main');
-    
-    
-  },
-  
-  
-  
-};
+}
 
-
-//Add and start the 'mainState' to start the game
+//add and start the 'mainState' to start the game
 game.state.add('main', mainState);
 game.state.start('main');
